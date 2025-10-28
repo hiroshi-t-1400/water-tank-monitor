843c063eaf9d4918baf3898ffb58c0879e3a2b19
hiroshi-t-1400 <hiroshi.tsukimori@gmail.com>
2025-10-28 22:11:14 +0900

    apply_TALL stack
    TALLスタックを導入（Tailwind，Apline.js,Laravel）Livewireは無し
    docker desktop for windowsとWSL環境のためか、ポートの渡しがうまくかないので、ホストからの接続を全許可する設定にした@vite.config.js

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+// import './bootstrap';
+
+// Alpine.jsの導入
+import Alpine from 'alpinejs';
+
+// Alpineをグローバルなwindowsオブジェクトに登録
+window.Alpine = Alpine;
+
+// Alpineの起動
+Alpine.start();
--- src/vite.config.js
+++ src/vite.config.js
@@ -5,9 +5,27 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
+            // ビルドするフロントエンドのエントリーポイントを指定します。イミフ
             input: ['resources/css/app.css', 'resources/js/app.js'],
+            // 開発サーバー起動時のHMRを適切に動作させるための設定
             refresh: true,
         }),
         tailwindcss(),
     ],
+    // ★Docker環境でHMRを有効にするための設定（重要）
+    server: {
+        // HMRプロキシ設定
+        hmr: {
+            // host: 'localhost', // 外部からアクセスできるホスト名
+            host: '192.168.11.20',
+            // port: 5173, // 必要であればポートも指定
+        },
+        // Dockerホストへのバインド設定
+        watch: {
+            usePolling: true,
+        },
+        // Docker desktop for windows ,WSLの環境のためかwebsocket通信の段階でポート接続が上手く解決できないっぽい
+        // ホストからのアクセスを全て許可として解決、本番サーバーではセキュリティ面で不可
+        host: true,
+    }
 });
