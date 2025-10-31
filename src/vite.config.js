b5bb79c5306e8619d3710d2c25f0d76bedde931a
hiroshi-t-1400 <hiroshi.tsukimori@gmail.com>
2025-10-31 22:08:15 +0900

    apply_vite_tailwindcss
    とりあえずviteとtailwindcssは導入完了

--- src/vite.config.js
+++ src/vite.config.js
@@ -5,9 +5,26 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
+            // ビルドするフロントエンドのエントリーポイントを指定します
             input: ['resources/css/app.css', 'resources/js/app.js'],
+            // 開発サーバー起動時のHMRを適切に動作させるための設定
             refresh: true,
         }),
         tailwindcss(),
     ],
+    // Docker環境でHMRを有効にするための設定（重要）
+    server: {
+        // HRMプロキシ設定
+        hmr: {
+            // 外部からアクセスできるホスト名を設定
+            host: "192.168.11.20",
+            // 必要であればポートも指定、デフォルト値なので今回はコメントアウト
+            // port: 5173
+        },
+        watch: {
+            usePolling: true,
+        },
+        // viteにアクセスできなくなるので開発環境ではすべてのアクセスにtrueを返す
+        host: true,
+    }
 });
